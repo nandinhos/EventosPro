5190a17216f55e14047b054b63dbc6899f677230
Nando Dev <nandinhos@gmail.com>
2026-03-23 04:38:36 -0300

    fix(init.sh): remove docker exec wrap to allow execution from within the container

--- init.sh
+++ init.sh
@@ -1,26 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-# Este script deve ser rodado no servidor VPS
-# Ele entra automaticamente no container da aplicação e prepara
-# a base de dados (instalando as dependências do MYSQL/Mariadb)
+# Este script deve ser rodado DENTRO do container da aplicação.
+# Ele prepara a base de dados (instalando as dependências do MYSQL/Mariadb)
 # e configurando a conta dos Super Admins para restaurar backups.
 
-echo "Iniciando configuração no container eventosprofssdevcombr-app-1..."
+echo "==> Atualizando e Instalando MySQL CLI / MariaDB Connector..."
+apk update && apk add mysql-client mariadb-connector-c
 
-docker exec -it --user root eventosprofssdevcombr-app-1 sh -c '
-  echo "==> Atualizando e Instalando MySQL CLI / MariaDB Connector..."
-  apk update && apk add mysql-client mariadb-connector-c
+echo "==> Assumindo permissões seguras (www-data) para rodar o Laravel..."
+su -s /bin/sh www-data -c "
+  echo \"==> Rodando Migrações do zero...\"
+  php artisan migrate:fresh --force
   
-  echo "==> Assumindo permissões seguras (www-data) para rodar o Laravel..."
-  su -s /bin/sh www-data -c "
-    echo \"==> Rodando Migrações do zero...\"
-    php artisan migrate:fresh --force
-    
-    echo \"==> Inserindo seus usuários Admin Angélica e Nando...\"
-    php artisan db:seed --force
-    
-    echo \"==> Limpando Cache do Sistema...\"
-    php artisan optimize:clear
-  "
-  echo "==> OK! Tudo finalizado com sucesso."
-'
+  echo \"==> Inserindo seus usuários Admin Angélica e Nando...\"
+  php artisan db:seed --force
+  
+  echo \"==> Limpando Cache do Sistema...\"
+  php artisan optimize:clear
+"
+echo "==> OK! Tudo finalizado com sucesso."
